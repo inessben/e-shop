840c7cb1d3060416b8ae9217667a20fb1159cf22
Bel-kun <bellaire.vivien22@gmail.com>
2023-03-23 21:13:31 -0400

    First setup initialisation with vite + php + three.js + favicon

---
+++ sources/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/font.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./images/favicon/site.webmanifest">
+    <link rel="mask-icon" href="./images/favicon/safari-pinned-tab.svg" color="#000000">
+    <meta name="msapplication-TileColor" content="#121212">
+    <meta name="theme-color" content="#121212">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>E-shop</title>
+  </head>
+  <body>
+
+
+    <script type="module" src="./main.js"></script>
+
+  </body>
+</html>
+
